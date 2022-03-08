15fed1f854e9650e053af4c0c26b617d4eb4e435
nguyen xuan thanh <hd13112011@gmail.com>
2022-03-08 23:38:07 +0700

    sua doi thanh.js

--- thanh.js
+++ thanh.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 export function Check() {
-  ;<View></View>
+  ;<View>
+    <Text>Thanhf check</Text>
+  </View>
 }
